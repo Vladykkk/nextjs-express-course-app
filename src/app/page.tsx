644e90fcd9926271d86acab67c914804a7e02ad3
Vladyslav Rybak <rubakvlad7@gmail.com>
2024-06-06 11:47:28 +0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import styles from "./page.module.css";
+import course from "./course.json";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <ul>
+        {course.lessons.map((lesson) => {
+          return (
+            <li key={lesson.name}>
+              <h2>{lesson.title}</h2>
+              <p>{lesson.points}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
+}
